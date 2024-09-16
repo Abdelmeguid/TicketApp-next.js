98b7638e411f60040e063644bf480e7d16e90e66
Abdelmeguid <siegen567@gmail.com>
2024-09-16 17:31:22 +0300

    solve prisma proplems8

--- prisma/db.ts
+++ prisma/db.ts
@@ -1,15 +1,30 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient();
+// };
+
+// declare global {
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+// }
+
+// const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+// export default prisma;
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 import { PrismaClient } from "@prisma/client";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+// Ensure the Prisma Client is initialized only once to avoid unnecessary connections
+let prisma: PrismaClient;
 
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton();
-
 export default prisma;
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
